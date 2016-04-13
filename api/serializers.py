dcdc486824b040aae7b68d189fcb43304f3c316a
Diogo Lima <diogo.lima@stormsec.com.br>
2016-04-13 15:35:40 -0300

    corrigido o serializer msg

--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,10 @@ from .models import Mensagem, Identidade
 
 
 class MensagemSerializer(serializers.HyperlinkedModelSerializer):
+    sender = serializers.SlugField(source='remetente')
+    receipt = serializers.SlugField(source='destinatario')
+    date = serializers.DateTimeField(source='data', read_only=True)
+    text = serializers.CharField(source='texto')
     class Meta:
         model = Mensagem
-        fields = ('remetente', 'destinatario', 'data', 'texto')
+        fields = ('sender', 'receipt', 'date', 'text')
