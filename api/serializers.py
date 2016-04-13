951d744f6eea8c700ce7723f41349f902205baa7
Diogo Lima <diogo.lima@stormsec.com.br>
2016-04-13 13:43:47 -0300

    adicionado rest framework (2)

--- api/serializers.py
+++ api/serializers.py
@@ -4,9 +4,9 @@ from .models import Mensagem, Identidade
 
 
 class MensagemSerializer(serializers.HyperlinkedModelSerializer):
-    sender = serializers.HyperlinkedRelatedField(source='remetente')
-    receipt = serializers.HyperlinkedRelatedField(source='destinatario')
-    text = serializers.TextField(source='texto')
+    sender = serializers.CharField(source='remetente.nome')
+    receipt = serializers.CharField(source='destinatario.nome')
+    text = serializers.CharField(source='texto')
     date = serializers.DateTimeField(source='data')
 
     class Meta:
