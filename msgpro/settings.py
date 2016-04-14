5b470445000daee30a6f8a4ad9300324bc9ec012
Diogo Lima <diogo.lima@stormsec.com.br>
2016-04-14 15:01:45 -0300

    CORS

--- msgpro/settings.py
+++ msgpro/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'corsheaders',
+    'api',
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -128,6 +130,10 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     #'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
 }
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 try:
     from local_settings import *
