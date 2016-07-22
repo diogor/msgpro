ee76d37814227ac980eefdd5950726408c4dc32a
Diogo Lima <diogo.lima@stormsec.com.br>
2016-07-22 18:14:02 -0300

    envio de mensagens

--- api/models.py
+++ api/models.py
@@ -7,22 +7,24 @@ from django.db import models
 class Identidade(models.Model):
     nome = models.SlugField(unique=True)
     pubkey = models.TextField()
+    canal = models.CharField(max_length=20, unique=True)
+    online = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.nome
 
-
-TIPO = (('msg', 'Mensagem'), ('grp', 'Convite'))
+    def is_online(self):
+        return self.online
 
 
 class Mensagem(models.Model):
     remetente = models.ForeignKey(Identidade, related_name="caixa_de_saida")
     destinatario = models.ForeignKey(Identidade, related_name="caixa_de_entrada")
-    tipo = models.CharField(max_length=3, choices=TIPO, default='msg')
+    tipo = models.CharField(max_length=3, default='msg')
     texto = models.TextField()
     data = models.DateTimeField(auto_now_add=True)
     validade = models.IntegerField(default=3)
-    
+
     class Meta:
         ordering = ('-data',)
 
@@ -30,5 +32,5 @@ class Mensagem(models.Model):
         return self.texto
 
     def is_expired(self):
-        prazo = self.data + timedelta(days=self.prazo)
+        prazo = self.data + timedelta(days=self.validade)
         return datetime.now() > prazo
--- live/consumers.py
+++ live/consumers.py
@@ -1,12 +1,14 @@
 import json
 from random import randint
 from time import time
+from django.core import serializers
 from channels import Group
 from channels.sessions import channel_session
 
-from api.models import Identidade
+from api.models import Identidade, Mensagem
 
-# Connected to websocket.connect
+
+
 @channel_session
 def ws_connect(message):
     room = "{}.{}".format(str(time()), str(randint(100000, 999999)))
@@ -21,25 +23,46 @@ def ws_connect(message):
 def ws_message(message):
     room = message.channel_session['room']
     mensagem = json.loads(message.get('text'))
-    print mensagem
     tipo = mensagem.get('type')
 
-    if tipo == 'ident':
-        print "d"
+    if tipo == 'id':
         nome = mensagem.get('name')
         pubkey = mensagem.get('pubkey')
         ident, criado = Identidade.objects.get_or_create(nome=nome, pubkey=pubkey)
+        ident.online = True
+        ident.save()
+        message.channel_session['ident'] = ident
         if criado:
-            msg = {"type": "newuser", "name": nome}
+            msg = {"type": "nu", "name": nome}
         else:
-            msg = {"type": "existinguser", "name": nome}
+            msg = {"type": "eu", "name": nome}
 
         Group("%s" % room).send({"text": json.dumps(msg)})
 
+    if tipo == 'msg':
+        destinatario = mensagem.get('recipient')
+        remetente = message.channel_session.get('ident')
+        text = mensagem.get('text')
+        if remetente:
+            nova = Mensagem.objects.create(remetente=remetente.nome, destinatario=destinatario, texto=text)
 
-# Connected to websocket.disconnect
+            id_remetente = Identidade.objects.get(nome=destinatario)
+
+            if id_remetente.is_online():
+                canal_dest = id_remetente.canal
+                msg = serializers.serialize('json', nova)
+                Group(canal_dest).send({"text": msg})
+            else:
+                msg = {"type": 'str', "recipient": id_remetente.nome, "text": nova.texto}
+                Group(room).send({"text": json.dumps(msg)})
+
+
 @channel_session
 def ws_disconnect(message):
+    ident = message.channel_session.get('ident')
+    if ident:
+        ident.online = False
+        ident.save()
     try:
         Group("%s" % message.channel_session['room']).discard(message.reply_channel)
     except KeyError:
