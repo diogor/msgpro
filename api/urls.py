bd3f513558423dd1ed258be5cdb675fa40121de2
Diogo Lima <diogooo@gmail.com>
2017-09-19 13:26:45 -0300

    url de busca

--- api/urls.py
+++ api/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^inbox/(?P<nome>[\w-]+)/$', MensagemList.as_view()),
-    url(r'^ident-search/(?P<q>[\w-]+)/$', IdentidadeSearch.as_view()),
+    url(r'^search/ident/(?P<q>[\w-]+)/$', IdentidadeSearch.as_view()),
     url(r'^new/ident/$', IdentidadeCreate.as_view()),
 ]
